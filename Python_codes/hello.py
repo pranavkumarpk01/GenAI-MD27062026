96d98b4a86c5a7a51b5bc407b715a0598610e512
PRANAV <pranav@PRANAVs-MacBook-Air.local>
2026-06-29 08:11:24 +0530

    Adding python codes

---
+++ Python_codes/hello.py
@@ -0,0 +1,6 @@
+
+print("I am learning Python programming.")
+print("Python is a versatile language used for web development, data analysis, artificial intelligence, and more.")
+
+name = "Pranav"
+print("My name is " ,name) 
