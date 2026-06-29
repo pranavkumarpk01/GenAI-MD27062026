6c009818bdc2652e6226a3078de309947eb956d9
PRANAV <pranav@PRANAVs-MacBook-Air.local>
2026-06-29 08:53:03 +0530

    datatypes

---
+++ Python_codes/datatypes.py
@@ -0,0 +1,18 @@
+#Four Baic Data types
+#Integer -> Any number (25, 1000, 1000000 , 400) -> int
+#Float -> Any Decimal kind (3.12. 4.222, 5.1111) -> float
+#String -> "Python", "Banglore", "Tiger" -. Str
+#Boolean ->True , False ->bool
+
+#Integer
+age = 22
+marks = 500
+name ="Pranav"
+Present = True
+height = 5.10
+
+print(type(age))
+print(type(marks))
+print(type(name))
+print(type(Present))
+print(type(height))
---
+++ Python_codes/input.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+programming_language = input("Enter your favorite programming language: ")
+print("Welcome", name)
+print( name , "like" , programming_language)
+
+#Everything entered using input() is stored as a String(str) by default
+
+
+#Assignment
+#Write a Python Program that takes:Name, age and city 
+#It should print o/p -> My name is Rahul , I am 20 years old and I live in Banglore..
