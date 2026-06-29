a14f87ee19c6fd4f0e554c5a604b719f31a2be71
pranavkumarpk01 <124168954+pranavkumarpk01@users.noreply.github.com>
2026-06-29 08:12:13 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-
-print("I am learning Python programming.")
-print("Python is a versatile language used for web development, data analysis, artificial intelligence, and more.")
-
-name = "Pranav"
-print("My name is " ,name) 
