1cab44495a15ca068db44340e894716b44deeac8
PRANAV <pranav@PRANAVs-MacBook-Air.local>
2026-06-30 08:51:08 +0530

    operators

---
+++ Python_codes/operators.py
@@ -0,0 +1,68 @@
+#Arithmetic operators
+#  a + b
+#  operand(a) operator(+) operand(b)
+
+#  + -> Addition
+#  - -> Subtraction
+#  * -> MUltiplication
+#  / -> Division
+#  % -> Modulus
+
+# 10/2 -> 5 (Quotient)
+# 10%2 -> 0 (remainder)
+
+# 2)10(5 -> Quotient
+# - 10
+# --------
+#   0 -> Remainder
+
+#Assignment Operators
+# x = 10
+# print(x)
+
+# x +=5  
+# print(x)
+
+# x -=2 
+# print(x)
+
+#Comparison Operators
+#It is used to compare two values and will always return True or False 
+
+# a = 10
+# b = 20
+# print( a == b) 
+# print( a != b) 
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+
+#Logical Operators
+# and
+# or
+
+# Truth table (AND)
+
+# A.    B.      Result
+# T.    T.      T
+# T.    F.      F
+# F.    T.      F
+# F.    F.      F
+
+# Truth table (OR)
+
+# A.    B.      Result
+# T.    T.      T
+# T.    F.      T
+# F.    T.      T
+# F.    F.      F
+
+#Program 
+
+attendance = 80
+medical = False
+
+print(attendance >= 75 and medical)
+# print( True or False) 
+
