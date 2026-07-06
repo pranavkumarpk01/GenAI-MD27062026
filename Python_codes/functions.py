8cbcb6f98097eafc3cf3420445ed69742b30a4b6
pranavkumarpk01 <pranav@PRANAVs-MacBook-Air.local>
2026-07-06 08:10:27 +0530

    functions .py

---
+++ Python_codes/functions.py
@@ -0,0 +1,49 @@
+#A fuction is a block of reusable code that performs a specific task
+#Instead of writing the same code multiple times, we write it once inside a fucntion and call it whenever needed 
+
+#def function_name():
+    #code
+
+# def greet():
+#     print("welcome to python")
+    
+#Function calling
+# greet()     
+
+# def display():
+#     print("Hello")
+#     print("Good Morning")
+
+# display()
+
+# print("Welcome")
+# print("Welcome")
+# print("Welcome")
+
+# def welcome():
+#     print("Welcome")
+
+
+# welcome()
+# welcome()
+# welcome()    
+
+#Parameters
+#This are the variables written inside the function definition
+#def function(parameter):
+
+# def greet(name):
+#     print("Hello" , name )
+
+# greet("Pranav")    
+# "Pranav" -> Arguement
+# name -> parameter
+
+#multiple params
+def student(name: str , age: int):
+    print(name)
+    print(age)
+
+student("Pranav",24)
+student("Manish",32)
+student("Shashi",12)
